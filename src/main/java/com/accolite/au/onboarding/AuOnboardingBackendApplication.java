2871968ff26cd93669e2ea4fb41385d5e752ecee
panktit <panktit07@gmail.com>
2020-05-28 16:28:15 +0530

    :tada:initial commit

---
+++ src/main/java/com/accolite/au/onboarding/AuOnboardingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.accolite.au.onboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuOnboardingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuOnboardingBackendApplication.class, args);
+		System.out.println(">>>>>> Run successful!");
+	}
+}
---
+++ src/test/java/com/accolite/au/onboarding/AuOnboardingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accolite.au.onboarding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuOnboardingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
